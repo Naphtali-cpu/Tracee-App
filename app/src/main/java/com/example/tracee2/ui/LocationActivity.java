b7aef3ef0667c4b3b1eb03b755c5dd59311d5ff1
Naphtali-cpu <naphtali919@gmail.com>
2021-09-10 18:08:28 +0300

    Added Picaso on images

--- app/src/main/java/com/example/tracee2/ui/LocationActivity.java
+++ app/src/main/java/com/example/tracee2/ui/LocationActivity.java
@@ -1,11 +1,12 @@
 package com.example.tracee2.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import butterknife.BindView;
@@ -14,15 +15,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
-
-import com.example.tracee2.MyRestaurantsArrayAdapter;
-import com.example.tracee2.MyRestaurantsArrayAdapter;
 import com.example.tracee2.R;
+import com.example.tracee2.adapters.RestaurantListAdapter;
 import com.example.tracee2.models.Business;
-import com.example.tracee2.models.Category;
 import com.example.tracee2.models.YelpBusinessesSearchResponse;
 import com.example.tracee2.network.YelpApi;
 import com.example.tracee2.network.YelpClient;
@@ -31,10 +26,14 @@ import java.util.List;
 
 public class LocationActivity extends AppCompatActivity {
     private static final String TAG = LocationActivity.class.getSimpleName();
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.errorTextView) TextView mErrorTextView;
     @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    private RestaurantListAdapter mAdapter;
+
+    public List<Business> restaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,44 +41,26 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
         ButterKnife.bind(this);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(LocationActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
-
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Trails near : " + location);
 
         YelpApi client = YelpClient.getClient();
+
         Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "trails");
 
         call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
             @Override
             public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
-
                 hideProgressBar();
 
                 if (response.isSuccessful()) {
-                    List<Business> restaurantsList = response.body().getBusinesses();
-                    String[] restaurants = new String[restaurantsList.size()];
-                    String[] categories = new String[restaurantsList.size()];
-
-                    for (int i = 0; i < restaurants.length; i++){
-                        restaurants[i] = restaurantsList.get(i).getName();
-                    }
-
-                    for (int i = 0; i < categories.length; i++) {
-                        Category category = restaurantsList.get(i).getCategories().get(0);
-                        categories[i] = category.getTitle();
-                    }
-
-                    ArrayAdapter adapter
-                            = new MyRestaurantsArrayAdapter(LocationActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
-                    mListView.setAdapter(adapter);
+                    restaurants = response.body().getBusinesses();
+                    mAdapter = new RestaurantListAdapter(LocationActivity.this, restaurants);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager =
+                            new LinearLayoutManager(LocationActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
 
                     showRestaurants();
                 } else {
@@ -89,7 +70,6 @@ public class LocationActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
-                Log.e(TAG, "onFailure: ",t );
                 hideProgressBar();
                 showFailureMessage();
             }
@@ -107,8 +87,7 @@ public class LocationActivity extends AppCompatActivity {
     }
 
     private void showRestaurants() {
-        mListView.setVisibility(View.VISIBLE);
-        mLocationTextView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 
     private void hideProgressBar() {
