7754f666231bc4faf15c6b3ce020743910ceadeb
Naphtali-cpu <naphtali919@gmail.com>
2021-09-21 18:51:10 +0300

    Added other Activitites for bottom bar

--- app/src/main/java/com/example/tracee2/ui/Dashboard.java
+++ app/src/main/java/com/example/tracee2/ui/Dashboard.java
@@ -1,47 +1,73 @@
 package com.example.tracee2.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-
 import com.example.tracee2.Constants;
 import com.example.tracee2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
-
 import butterknife.*;
 import butterknife.ButterKnife;
 
 public class Dashboard extends AppCompatActivity implements View.OnClickListener{
+    private CardView mHiking, mCycling, mCamping, mSwimming, mTravel, mWild;
+
+    public ImageView card1;
     Button btnLogOut;
     Button mbottomNavigationView;
     FirebaseAuth mAuth;
     @BindView(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-//    @BindView(R.id.hiking) Button mHiking;
-//    @BindView(R.id.cycling) Button mCycling;
-//    @BindView(R.id.imageview3) Button mCamping;
-//    @BindView(R.id.imageview4) Button mSwimming;
-//    @BindView(R.id.imageview5) Button mTravel;
-//    @BindView(R.id.imageview6) Button mWildTour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        return true;
+
+                    case R.id.places:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Places.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
         ButterKnife.bind(this);
 
         ImageView imageView = findViewById(R.id.imageview);
@@ -53,7 +79,7 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
         ImageView imageView7 = findViewById(R.id.imageview7);
         ImageView imageView8 = findViewById(R.id.imageview8);
         ImageView imageView9 = findViewById(R.id.imageview9);
-        ImageView imageView10 = findViewById(R.id.imageview10);
+//        ImageView imageView10 = findViewById(R.id.imageview10);
         String url = "https://images.pexels.com/photos/7107622/pexels-photo-7107622.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
         String url2 = "https://images.pexels.com/photos/1443527/pexels-photo-1443527.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
         String url3 = "https://images.pexels.com/photos/5914157/pexels-photo-5914157.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
@@ -74,7 +100,63 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
         Picasso.get().load(url7).placeholder(R.drawable.ic_launcher_foreground).into(imageView7);
         Picasso.get().load(url8).placeholder(R.drawable.ic_launcher_foreground).into(imageView8);
         Picasso.get().load(url9).placeholder(R.drawable.ic_launcher_foreground).into(imageView9);
-        Picasso.get().load(url10).placeholder(R.drawable.ic_launcher_foreground).into(imageView10);
+//        Picasso.get().load(url10).placeholder(R.drawable.ic_launcher_foreground).into(imageView10);
+
+        mHiking = (CardView) findViewById(R.id.c1);
+        mCycling = (CardView) findViewById(R.id.c2);
+        mCamping = (CardView) findViewById(R.id.c3);
+        mSwimming = (CardView) findViewById(R.id.c4);
+        mTravel = (CardView) findViewById(R.id.c5);
+        mWild = (CardView) findViewById(R.id.c6);
+
+
+        mHiking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, Hiking.class);
+                startActivity(intent);
+            }
+
+        });
+        mCamping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, Camping.class);
+                startActivity(intent);
+            }
+
+        });
+        mSwimming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, Swimming.class);
+                startActivity(intent);
+            }
+
+        });
+        mTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, Travel.class);
+                startActivity(intent);
+            }
+
+        });
+
+        mCycling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Cycling.class);
+                startActivity(intent);
+            }
+        });
+        mWild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Wild.class);
+                startActivity(intent);
+            }
+        });
 
         btnLogOut = findViewById(R.id.btnLogout);
         mAuth = FirebaseAuth.getInstance();
@@ -89,6 +171,7 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
 
     @Override
     public void onClick(View v){
+
         if (v == mFindRestaurantsButton) {
             Intent intent = new Intent(Dashboard.this, LocationActivity.class);
             startActivity(intent);
@@ -97,14 +180,6 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
             Intent intent = new Intent(Dashboard.this, SavedLocationListActivity.class);
             startActivity(intent);
         }
-//        if (v == mHiking) {
-//            Intent intent = new Intent(Dashboard.this, Hiking.class);
-//            startActivity(intent);
-//        }
-//        if (v == mCycling) {
-//            Intent intent = new Intent(Dashboard.this, Cycling.class);
-//            startActivity(intent);
-//        }
     }
 
 }
--- app/src/main/java/com/example/tracee2/ui/Hiking.java
+++ app/src/main/java/com/example/tracee2/ui/Hiking.java
@@ -1,16 +1,39 @@
 package com.example.tracee2.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.tracee2.R;
+import com.squareup.picasso.Picasso;
 
 public class Hiking extends AppCompatActivity {
+    private ImageView mHiking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiking);
+        ImageView imageView = findViewById(R.id.imageView5);
+        String url = "https://images.pexels.com/photos/5065321/pexels-photo-5065321.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+        Picasso.get().load(url).placeholder(R.drawable.ic_launcher_foreground).into(imageView);
+
+        mHiking = (ImageView) findViewById(R.id.imageView2);
+
+        mHiking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Hiking.this, Dashboard.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+
     }
 }
